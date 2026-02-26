f4d7f27978d3c30bac8b08a15cdc5fafac11d17b
Dreamy <dreamy@dreamydrawlab.ai>
2026-02-26 11:00:42 -0700

    chore: revert URLs back to dreamyunit204.github.io (2026-02-26)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://dreamydrawphx.github.io',
+  site: 'https://dreamyunit204.github.io',
   base: '/dreamy-draw-weekly/',
   output: 'static',
   markdown: {
